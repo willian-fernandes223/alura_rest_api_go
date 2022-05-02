790f58719e0ec217ebc15fd2f964d2d0de6a8598
Willian <fernandes_223@hotmail.com>
2022-05-02 14:07:03 -0300

    Remoção da linha de teste

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ func main() {
 		models.Personalidades = []models.Personalidade{
 			{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 			{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
-
-			teste git
 		}*/
 
 	database.ConectaComBD()
